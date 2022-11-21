d03e22b489737fa8163005a50a93cbf596ec7e41
Stacey Andrew <andrew23.gonzaga@gmail.com>
2022-11-21 20:55:03 +0800

    initial commit

---
+++ exercise3.py
@@ -0,0 +1,9 @@
+# initializing variables
+minutes = 60  # minutes in an hour
+hours = 24  # hours in a day
+days = 365  # days in a year
+
+minInYear = minutes * hours * days
+
+# output
+print("The number of minutes in a year is", minInYear)
